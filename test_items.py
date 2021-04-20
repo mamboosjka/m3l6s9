19712f2aa100ea4e2603a1dd93816834d3603848
Alex Zapolsky <mamboosjka@gmail.com>
2021-04-20 03:00:49 +0300

    Added Try Except Finally block to test for get assert when test will fail.

--- test_items.py
+++ test_items.py
@@ -1,7 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_add_to_basket_button_is_present_form(browser):
     browser.get(link)
-    add_button = browser.find_element_by_css_selector("button.btn.btn-add-to-basket[type='submit']")
-    assert bool(add_button) == True, "Cant find 'Add to Basket' button."
+    try:
+        add_button = browser.find_element_by_css_selector("button.btn.btn-add-to-basket[type='submit']")
+    except NoSuchElementException:
+        add_button = False
+    finally:
+        assert add_button, "Cant find 'Add to Basket' button."
